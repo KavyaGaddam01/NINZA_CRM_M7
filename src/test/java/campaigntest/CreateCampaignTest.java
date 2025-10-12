9cb40e56f1f7991994004e2dd56fcd2b834b8932
KavyaGaddam01 <kavyagaddam09@gmail.com>
2025-10-12 09:30:32 +0530

    Added test to print stat

--- src/test/java/campaigntest/CreateCampaignTest.java
+++ src/test/java/campaigntest/CreateCampaignTest.java
@@ -66,7 +66,7 @@ public class CreateCampaignTest extends BaseClass {
 		wLib.waitUntilElementToBeVisible(driver, toastMsg);
 		Assert.assertTrue(toastMsg.getText().contains(CAMPAIGN_NAME));
 		hp.getCloseToastMsg().click();
-		System.out.println("CreateCampaignWithStatus");
+		System.out.println("CreateCampaignWithStatustest");
 
 	}
 
